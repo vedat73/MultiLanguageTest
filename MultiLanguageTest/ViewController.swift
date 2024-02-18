28ef9e79cba3c576146fde7b18b605ac643888cc
Vedat OZLU <vedat.ozlu@outlook.com>
2024-02-18 11:22:12 +0300

    first commit

--- MultiLanguageTest/ViewController.swift
+++ MultiLanguageTest/ViewController.swift
@@ -7,13 +7,46 @@
 
 import UIKit
 
+let LocalizeUserDefaultKey = "LocalizeUserDefaultKey"
+var LocalizeDefaultLanguage = "en"
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var testLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        LocalizeDefaultLanguage = UserDefaults.standard.string(forKey: LocalizeUserDefaultKey) ?? "en"
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        refreshLanguage()
+    }
+    
+    private func refreshLanguage() {
+        testLabel.text = "message".translated()
     }
 
-
+    @IBAction func turkishButtonTapped(_ sender: Any) {
+        LocalizeDefaultLanguage = "tr"
+        UserDefaults.standard.setValue(LocalizeDefaultLanguage, forKey: LocalizeUserDefaultKey)
+        refreshLanguage()
+    }
+    
+    @IBAction func englishButtonTapped(_ sender: Any) {
+        LocalizeDefaultLanguage = "en"
+        UserDefaults.standard.setValue(LocalizeDefaultLanguage, forKey: LocalizeUserDefaultKey)
+        refreshLanguage()
+    }
 }
 
+extension String {
+    func translated() -> String {
+        if let path = Bundle.main.path(forResource: LocalizeDefaultLanguage, ofType: "lproj"), let bundle = Bundle(path: path) {
+            return NSLocalizedString(self, bundle: bundle, comment: "")
+        }
+        
+        return ""
+    }
+}
